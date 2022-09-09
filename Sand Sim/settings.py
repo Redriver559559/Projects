de9e3cf7a0c64d9cc71b80463ce60a3024fc2dc1
Gavin Tanner <69123362+Redriver559559@users.noreply.github.com>
2022-09-08 21:40:46 -0700

    Create settings.py

---
+++ Sand Sim/settings.py
@@ -0,0 +1,5 @@
+cell_size = 10
+width, height = 600, 400
+grid_height, grid_width = height//cell_size, width//cell_size
+grid = [[0 for _ in range(grid_width)] for _ in range(grid_height)]
+events = {}
